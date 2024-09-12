e5203d1779a66f7e59514d6b54429ccde4bab97f
nikachitava <chitava18.official@gmail.com>
2024-09-12 14:50:40 +0400

    create rooms number buttons

--- src/app/components/custom/Dropdown/RoomsNumberContent.tsx
+++ src/app/components/custom/Dropdown/RoomsNumberContent.tsx
@@ -6,21 +6,14 @@ const RoomsNumberContent = () => {
 		<div className="absolute min-w-[282px] flex flex-col gap-6 mt-[10px] border-[1px] border-[#DBDBDB] p-6 bg-white z-10">
 			<h1>საფასო კატეგორია</h1>
 			<div className="grid grid-cols-3 gap-4">
-				<div className="w-10 h-10 flex justify-center items-center rounded-md border-[1px] border-[#808A93] text-greytext cursor-pointer ">
-					1
-				</div>
-				<div className="w-10 h-10 flex justify-center items-center rounded-md border-[1px] border-[#808A93] text-greytext cursor-pointer">
-					2
-				</div>
-				<div className="w-10 h-10 flex justify-center items-center rounded-md border-[1px] border-[#808A93] text-greytext cursor-pointer">
-					3
-				</div>
-				<div className="w-10 h-10 flex justify-center items-center rounded-md border-[1px] border-[#808A93] text-greytext cursor-pointer">
-					4
-				</div>
-				<div className="w-10 h-10 flex justify-center items-center rounded-md border-[1px] border-[#808A93] text-greytext cursor-pointer">
-					5
-				</div>
+				{[1, 2, 3, 4, 5].map((item) => (
+					<button
+						className="w-10 h-10 flex justify-center items-center rounded-md border-[1px] border-[#808A93] text-greytext cursor-pointer "
+						value={item}
+					>
+						{item}
+					</button>
+				))}
 			</div>
 			<div className="flex justify-end items-center">
 				<CustomButtom title={"არჩევა"} fill />
